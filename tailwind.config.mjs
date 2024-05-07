487edf184ba5d02f15dc146033183c7561c9bfa0
Diego <diegoyon@outlook.com>
2024-05-07 15:27:10 -0600

    add custom font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
