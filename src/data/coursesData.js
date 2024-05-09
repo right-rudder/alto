eb930686401a92f4cdbff662d9555e91e3d2d282
Diego <diegoyon@outlook.com>
2024-05-09 00:37:43 -0600

    add courses overview section

---
+++ src/data/coursesData.js
@@ -0,0 +1,20 @@
+export const courses = [
+  {
+    name: "Private Pilot",
+    duration: "6 months",
+    description:
+      "Become a private pilot and learn to fly an aircraft with the highest level of safety and professionalism.",
+  },
+  {
+    name: "Commercial Pilot",
+    duration: "12 months",
+    description:
+      "Become a commercial pilot and learn to fly an aircraft with the highest level of safety and professionalism.",
+  },
+  {
+    name: "Airline Transport Pilot",
+    duration: "18 months",
+    description:
+      "Become an airline transport pilot and learn to fly an aircraft with the highest level of safety and professionalism.",
+  },
+];
