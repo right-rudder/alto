85e1461e7ede272ce621989e0f4c5f66c8ea56d1
Diego <diegoyon@outlook.com>
2024-05-19 23:53:19 -0600

    add acquisition sales index

---
+++ src/data/aircrafts.js
@@ -0,0 +1,57 @@
+const aircrafts = [
+  {
+    data: {
+      name: "N7085G",
+      price: "$500,000",
+      usableFuel: "38 gal",
+      model: "Cessna 172K",
+      year: "1998",
+      equipment: [
+        "Garmin106 CDI Indicator",
+        "Garmin GNX 375 Navigator/Transponder",
+        "Dual Bendix/King KX 155 Nav/Comm",
+      ],
+      imagePath: "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
+      alt: "Cessna 172K",
+      imagesArray: [
+        "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
+        "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
+      ],
+      description: [
+        "The Cessna 172 is the gold standard for new pilots. Its forgiving flight characteristics and robust reliability make it the perfect choice for initial flight training. The high-wing design offers exceptional stability and visibility, boosting student confidence as they master essential flying skills. The aircraft responds gently to control inputs and recovers smoothly from errors, instilling the poise needed to become a competent pilot. With widespread availability and easy access to maintenance expertise, the Cessna 172 keeps operational costs low, allowing for consistent training schedules.",
+        "Beyond its physical attributes, the aircraft is equipped with modern avionics and instrumentation, giving students a real-world flying experience. Its versatility extends from solo flights to instrument training, making the Cessna 172 a fundamental tool in developing proficient pilots",
+      ],
+    },
+    slug: "N7085G",
+  },
+  {
+    data: {
+      name: "N7994X",
+      price: "$500,000",
+      usableFuel: "38 gal",
+      model: "Cessna 172B",
+      year: "1998",
+      equipment: [
+        "Dual G5",
+        "PS PAR 200A Audiopanel/Comm",
+        "Garmin 430",
+        "Garmin GTX 330 Transponder",
+        "JPI Fuel Flow Gauge",
+      ],
+      imagePath: "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
+      alt: "Cessna 172K",
+      imagesArray: [
+        "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
+      ],
+      description: [
+        "The Cessna 172 is the gold standard for new pilots. Its forgiving flight characteristics and robust reliability make it the perfect choice for initial flight training. The high-wing design offers exceptional stability and visibility, boosting student confidence as they master essential flying skills. The aircraft responds gently to control inputs and recovers smoothly from errors, instilling the poise needed to become a competent pilot. With widespread availability and easy access to maintenance expertise, the Cessna 172 keeps operational costs low, allowing for consistent training schedules.",
+        "Beyond its physical attributes, the aircraft is equipped with modern avionics and instrumentation, giving students a real-world flying experience. Its versatility extends from solo flights to instrument training, making the Cessna 172 a fundamental tool in developing proficient pilots",
+      ],
+      hasRibbon: true,
+      ribbonTitle: "sold",
+    },
+    slug: "N7994X",
+  },
+];
+
+export default aircrafts;
