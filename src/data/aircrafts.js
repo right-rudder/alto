0f4022bd0308d7156ed82b786b8e3fadc994aa6b
Diego <diegoyon@outlook.com>
2024-05-20 15:26:29 -0600

    add aircraft images

--- src/data/aircrafts.js
+++ src/data/aircrafts.js
@@ -1,35 +1,32 @@
 const aircrafts = [
   {
     data: {
-      name: "N7085G",
+      name: "N110GA",
       price: "$500,000",
       usableFuel: "38 gal",
-      model: "Cessna 172K",
-      year: "1998",
+      model: "Beechjet 400 ",
+      year: "1988",
       equipment: [
         "Garmin106 CDI Indicator",
         "Garmin GNX 375 Navigator/Transponder",
         "Dual Bendix/King KX 155 Nav/Comm",
       ],
-      imagePath: "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
-      alt: "Cessna 172K",
-      imagesArray: [
-        "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
-        "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
-      ],
+      imagePath: "/src/assets/beechjet-400.jpeg",
+      alt: "Beechjet 400",
+      imagesArray: ["/src/assets/beechjet-400.jpeg"],
       description: [
         "The Cessna 172 is the gold standard for new pilots. Its forgiving flight characteristics and robust reliability make it the perfect choice for initial flight training. The high-wing design offers exceptional stability and visibility, boosting student confidence as they master essential flying skills. The aircraft responds gently to control inputs and recovers smoothly from errors, instilling the poise needed to become a competent pilot. With widespread availability and easy access to maintenance expertise, the Cessna 172 keeps operational costs low, allowing for consistent training schedules.",
         "Beyond its physical attributes, the aircraft is equipped with modern avionics and instrumentation, giving students a real-world flying experience. Its versatility extends from solo flights to instrument training, making the Cessna 172 a fundamental tool in developing proficient pilots",
       ],
     },
-    slug: "N7085G",
+    slug: "N110GA",
   },
   {
     data: {
-      name: "N7994X",
+      name: "N2201U",
       price: "$500,000",
       usableFuel: "38 gal",
-      model: "Cessna 172B",
+      model: "Beech Bonanza B36TC",
       year: "1998",
       equipment: [
         "Dual G5",
@@ -38,10 +35,63 @@ const aircrafts = [
         "Garmin GTX 330 Transponder",
         "JPI Fuel Flow Gauge",
       ],
-      imagePath: "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
-      alt: "Cessna 172K",
+      imagePath: "/src/assets/beech-bonanza-b36tc.jpg",
+      alt: "Beech Bonanza B36TC",
+      imagesArray: ["/src/assets/beech-bonanza-b36tc.jpg"],
+      description: [
+        "The Cessna 172 is the gold standard for new pilots. Its forgiving flight characteristics and robust reliability make it the perfect choice for initial flight training. The high-wing design offers exceptional stability and visibility, boosting student confidence as they master essential flying skills. The aircraft responds gently to control inputs and recovers smoothly from errors, instilling the poise needed to become a competent pilot. With widespread availability and easy access to maintenance expertise, the Cessna 172 keeps operational costs low, allowing for consistent training schedules.",
+        "Beyond its physical attributes, the aircraft is equipped with modern avionics and instrumentation, giving students a real-world flying experience. Its versatility extends from solo flights to instrument training, making the Cessna 172 a fundamental tool in developing proficient pilots",
+      ],
+      hasRibbon: true,
+      ribbonTitle: "sold",
+    },
+    slug: "N2201U",
+  },
+  {
+    data: {
+      name: "N6197B",
+      price: "$500,000",
+      usableFuel: "38 gal",
+      model: "Cessna 182A",
+      year: "1957",
+      equipment: [
+        "Dual G5",
+        "PS PAR 200A Audiopanel/Comm",
+        "Garmin 430",
+        "Garmin GTX 330 Transponder",
+        "JPI Fuel Flow Gauge",
+      ],
+      imagePath: "/src/assets/cessna-182a.jpg",
+      alt: "Cessna 182A",
+      imagesArray: ["/src/assets/cessna-182a.jpg"],
+      description: [
+        "The Cessna 172 is the gold standard for new pilots. Its forgiving flight characteristics and robust reliability make it the perfect choice for initial flight training. The high-wing design offers exceptional stability and visibility, boosting student confidence as they master essential flying skills. The aircraft responds gently to control inputs and recovers smoothly from errors, instilling the poise needed to become a competent pilot. With widespread availability and easy access to maintenance expertise, the Cessna 172 keeps operational costs low, allowing for consistent training schedules.",
+        "Beyond its physical attributes, the aircraft is equipped with modern avionics and instrumentation, giving students a real-world flying experience. Its versatility extends from solo flights to instrument training, making the Cessna 172 a fundamental tool in developing proficient pilots",
+      ],
+      hasRibbon: true,
+      ribbonTitle: "sold",
+    },
+    slug: "N6197B",
+  },
+  {
+    data: {
+      name: "N500TU",
+      price: "$500,000",
+      usableFuel: "38 gal",
+      model: "Aero Commander",
+      year: "1962",
+      equipment: [
+        "Dual G5",
+        "PS PAR 200A Audiopanel/Comm",
+        "Garmin 430",
+        "Garmin GTX 330 Transponder",
+        "JPI Fuel Flow Gauge",
+      ],
+      imagePath: "/src/assets/aero-commander-side.jpg",
+      alt: "Aero Commander",
       imagesArray: [
-        "/src/assets/N67797-cessna-flying-over-sundance-airport.jpg",
+        "/src/assets/aero-commander-front.jpg",
+        "/src/assets/aero-commander-side.jpg",
       ],
       description: [
         "The Cessna 172 is the gold standard for new pilots. Its forgiving flight characteristics and robust reliability make it the perfect choice for initial flight training. The high-wing design offers exceptional stability and visibility, boosting student confidence as they master essential flying skills. The aircraft responds gently to control inputs and recovers smoothly from errors, instilling the poise needed to become a competent pilot. With widespread availability and easy access to maintenance expertise, the Cessna 172 keeps operational costs low, allowing for consistent training schedules.",
@@ -50,7 +100,7 @@ const aircrafts = [
       hasRibbon: true,
       ribbonTitle: "sold",
     },
-    slug: "N7994X",
+    slug: "N500TU",
   },
 ];
 
