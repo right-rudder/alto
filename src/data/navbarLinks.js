6f7ce1ce3095b61b3fae8870f5ce89deb743d5e4
Diego <diegoyon@outlook.com>
2024-05-19 21:50:34 -0600

    separate ground school and ground training

--- src/data/navbarLinks.js
+++ src/data/navbarLinks.js
@@ -5,35 +5,60 @@ export const navbarLinks = [
     submenu: [],
   },
   {
-    name: "Our Courses",
+    name: "Ground School",
     submenu: [
       {
-        name: "Ground School",
-        link: "/courses/ground-school",
-      },
-      {
         name: "Private Pilot",
-        link: "/courses/private-pilot",
+        link: "/ground-school/private-pilot",
       },
       {
         name: "Instrument Rating",
-        link: "/courses/instrument-rating",
+        link: "/ground-school/instrument-rating",
       },
       {
         name: "Commercial Pilot",
-        link: "/courses/commercial-pilot",
+        link: "/ground-school/commercial-pilot",
       },
       {
         name: "Flight Instructor",
-        link: "/courses/flight-instructor",
+        link: "/ground-school/flight-instructor",
       },
       {
         name: "Multi-Engine Rating",
-        link: "/courses/multi-engine-rating",
+        link: "/ground-school/multi-engine-rating",
       },
       {
         name: "Airline Transport Pilot",
-        link: "/courses/airline-transport-pilot",
+        link: "/ground-school/airline-transport-pilot",
+      },
+    ],
+  },
+  {
+    name: "Flight Training",
+    submenu: [
+      {
+        name: "Private Pilot",
+        link: "/flight-training/private-pilot",
+      },
+      {
+        name: "Instrument Rating",
+        link: "/flight-training/instrument-rating",
+      },
+      {
+        name: "Commercial Pilot",
+        link: "/flight-training/commercial-pilot",
+      },
+      {
+        name: "Flight Instructor",
+        link: "/flight-training/flight-instructor",
+      },
+      {
+        name: "Multi-Engine Rating",
+        link: "/flight-training/multi-engine-rating",
+      },
+      {
+        name: "Airline Transport Pilot",
+        link: "/flight-training/airline-transport-pilot",
       },
     ],
   },
