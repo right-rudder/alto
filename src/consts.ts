2e0453ba953cb6ea29baa48b8baccfd8f7708581
Tim Jedrek <tim@timjedrek.com>
2024-05-24 11:04:38 -0500

    cta section styling

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const KEYWORDS =
   "flight school, flight training, pilot training, pilot school, flight lessons, flight instruction, flight instructor, flight school near me, flight training near me, pilot training near me, pilot school near me, flight lessons near me, flight instruction near me, flight instructor near me";
 
 export const PHONE_NUMBER = "(405) 789-1234";
-export const AIRPORT = "Sundance Airport (KHSD)";
+export const AIRPORT = "Sundance Airport";
 export const ADDRESS_LINE_1 = "13000 N Sara Rd";
 export const ADDRESS_LINE_2 = "Yukon, OK 73099";
 export const FACEBOOK_URL = "https://www.facebook.com/AltoFlightAcademy/";
