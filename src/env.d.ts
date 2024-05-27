75012cbd90e058fcd998cf3217ab3df032942df6
Diego <diegoyon@outlook.com>
2024-05-27 15:16:04 -0600

    hook quick start form to ghl

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly ENROLLMENT_FORM_WEBHOOK_URL: string;
+  readonly CONTACT_US_FORM_WEBHOOK_URL: string;
+  readonly QUIZ_FORM_WEBHOOK_URL: string;
+  readonly QUICK_START_FORM_WEBHOOK_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
