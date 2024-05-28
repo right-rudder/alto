8f3fa50b54a206b4aeb9355e853abb968f77ad98
Diego <diegoyon@outlook.com>
2024-05-27 22:35:28 -0600

    add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import alpinejs from "@astrojs/alpinejs";
@@ -9,6 +9,13 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react(), alpinejs(), partytown()]
+  site: "https://altoflight.com/",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    alpinejs(),
+    partytown(),
+  ],
 });
