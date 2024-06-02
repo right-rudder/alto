0be5d3beef74567821ba1c167556082e4f082096
Tim Jedrek <tim@timjedrek.com>
2024-06-02 02:57:36 -0500

    private pilot page start

--- src/data/privatePilotData.js
+++ src/data/privatePilotData.js
@@ -4,17 +4,17 @@ const privatePilotData = {
     siteDescription: "Obtaining your private pilot certificate is the first certification in your pilot training journey.  Having a private pilot certificate allows you to be pilot in command and also allows you to take passengers.  You may not fly in IMC (think cloudy weather), or fly for compensation or hire.  Start your private pilot training today in Oklahoma City!",
     title: "Private Pilot Training",
     hiddenField: "Private Pilot Flight Training",
-    date: "Starting November 21, 2024 - 4:00pm-6:00pm - 4 months",
+    date: "Book a discovery flight and start your pilot training today",
     price: "$1350",
     intro:
-      "In as little as 35 hours of flight training, you can become a private pilot. This will allow you to fly anywhere in the United States and is the first step of becoming a professional pilot! Being a private pilot has many advantages. You can take a loved one, children, friends, or anyone you want to any location. Imagine flying a spouse to Key West for dinner and back in time for the 11 o’clock news. All of this and more is possible with your private pilot license lorem ipsum dolor sit amet",
+      "If you're just starting out and learning how to fly, this is where you'll be.  The private pilot certificate, also infamously known as a 'pilot license' is the first certification you will receive in your pilot training journey.  You'll learn the fundamentals of flight: straight and level flight, turns, climbs, and descents. You'll also learn what all those gauges and screens mean.  Soon enough you'll know the difference between an attitude indicator and a directional gyro.  If all of that looks fun to you, we encourage you to reach out to us and schedule an introductory discovery flight.  It'll be your first flight lesson where you take controls of an airplane.",
     content: [
       "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet.",
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
     ],
     headerImage:
-      "/src/assets/student-passed-his-private-pilot-practical-exam.jpg",
-    headerAlt: "Chad Linville at Sundance Airport with his dad's Cessna 172",
+      "/src/assets/alto-flight-private-pilot-student-after-solo.jpg",
+    headerAlt: "An Alto Flight Academy private pilot student at Sundance Airport in front of a cessna 172 at Sundance Airport",
     primaryImage: "/src/assets/student-in-front-of-cessna.jpg",
     primaryAlt: "Courtland Stewart passed his private pilot exam",
     secondaryImage:
