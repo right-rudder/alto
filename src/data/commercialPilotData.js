a47f57315acbdf4addc4855281dc968874fbd64f
Tim Jedrek <tim@timjedrek.com>
2024-06-04 03:22:05 -0500

    now just adding stuff.. inst and com

--- src/data/commercialPilotData.js
+++ src/data/commercialPilotData.js
@@ -12,7 +12,7 @@ const commercialPilotData = {
       "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet.",
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
     ],
-    headerImage: "/src/assets/student-passed-his-commercial-pilot-exam.jpg",
+    headerImage: "/src/assets/commercial-pilot-landing-grass-runway-in-cedar-mills.jpg",
     headerAlt: "Kayden Russell passed his commercial pilot exam",
     primaryImage:
       "/src/assets/student-obtains-commercial-pilot-certificate.jpg",
--- src/data/instrumentRatingDataGroundSchool.js
+++ src/data/instrumentRatingDataGroundSchool.js
@@ -4,7 +4,7 @@ const instrumentRatingDataGroundSchool = {
     siteDescription: "Enroll in our upcoming instrument ground school.  This ground school will prepare you to take the instrument written exam.  The requirements for obtaining your instrument rating can be very technical and text-book heavy.  You'll need to learn how to read approach plates, STARs and SIDs, along with learning a whole new set of regulations that apply when flying in IMC (instrument meterological conditions).",
     title: "Instrument Ground School",
     hiddenField: "Instrument Rating Ground School",
-    date: "Starting November 21, 2024 - 4:00pm-6:00pm - 4 months",
+    date: "Next group ground school starts soon. Sign up to join",
     price: "$1350",
     intro:
       "In as little as 35 hours of flight training, you can become a private pilot. This will allow you to fly anywhere in the United States and is the first step of becoming a professional pilot! Being a private pilot has many advantages. You can take a loved one, children, friends, or anyone you want to any location. Imagine flying a spouse to Key West for dinner and back in time for the 11 o’clock news. All of this and more is possible with your private pilot license lorem ipsum dolor sit amet",
@@ -12,7 +12,7 @@ const instrumentRatingDataGroundSchool = {
       "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet.",
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
     ],
-    headerImage: "/src/assets/student-passed-his-instrument-rating.jpg",
+    headerImage: "/src/assets/IFR-chart-oklahoma-city-kokc-will-rogers-world.png",
     headerAlt: "Michael Salazar passed his instrument rating exam",
     primaryImage:
       "/src/assets/applicants-passed-their-instrument-instructor-checkrides.jpg",
