efea099a182205769809b0f9ebd7af157cfe185a
Tim Jedrek <tim@timjedrek.com>
2024-06-06 02:46:36 -0500

    reduce chroma

--- src/data/teamStaff.js
+++ src/data/teamStaff.js
@@ -8,7 +8,7 @@ const teamInstructors = [
   {
     name: "NAME",
     qualifications: ["Administration"],
-    imagePath: "/src/assets/team-member-2.jpg",
+    imagePath: "/src/assets/team-member-2b.jpg",
     //bio: "Hal Harris, our founder, has over 30 years of experience in aviation as our Chief Flight Instructor and Airline Transport Pilot. His passion for flight and dedication to teaching have inspired countless students to reach for the skies.",
   },
   {
