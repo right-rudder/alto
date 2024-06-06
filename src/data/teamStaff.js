7501575a1710a08da00e25e28cb33d839aabcb2c
Tim Jedrek <tim@timjedrek.com>
2024-06-06 02:18:53 -0500

    team page

---
+++ src/data/teamStaff.js
@@ -0,0 +1,22 @@
+const teamInstructors = [
+  {
+    name: "Beau",
+    qualifications: ["Aircraft and Powerplant Mechanic"],
+    imagePath: "/src/assets/team-member-1.jpg",
+    //bio: "Hal Harris, our founder, has over 30 years of experience in aviation as our Chief Flight Instructor and Airline Transport Pilot. His passion for flight and dedication to teaching have inspired countless students to reach for the skies.",
+  },
+  {
+    name: "NAME",
+    qualifications: ["Administration"],
+    imagePath: "/src/assets/team-member-2.jpg",
+    //bio: "Hal Harris, our founder, has over 30 years of experience in aviation as our Chief Flight Instructor and Airline Transport Pilot. His passion for flight and dedication to teaching have inspired countless students to reach for the skies.",
+  },
+  {
+    name: "NAME",
+    qualifications: ["Administration"],
+    imagePath: "/src/assets/team-member-3.jpg",
+    //bio: "Hal Harris, our founder, has over 30 years of experience in aviation as our Chief Flight Instructor and Airline Transport Pilot. His passion for flight and dedication to teaching have inspired countless students to reach for the skies.",
+  },
+];
+
+export default teamInstructors;
