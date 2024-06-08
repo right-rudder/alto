4a848728a5d2d6cbd93d90a90d7fe3f19d9ec603
Diego <diegoyon@outlook.com>
2024-06-08 10:04:09 -0600

    change image alt

--- src/data/commercialPilotData.js
+++ src/data/commercialPilotData.js
@@ -1,35 +1,37 @@
 const commercialPilotData = {
   data: {
     siteTitle: "Commercial Pilot Certificate | Flight School Oklahoma City",
-    siteDescription: "Start your pilot career now.  Obtain your commercial pilot certificate and get paid to fly.  Undergoing commercial pilot training means learning a new set of manuevers and regulations.  You'll learn how to be a more safe and precise pilot.  Enroll today at Alto Flight Academy and start your commercial pilot training in Oklahoma City.",
+    siteDescription:
+      "Start your pilot career now.  Obtain your commercial pilot certificate and get paid to fly.  Undergoing commercial pilot training means learning a new set of manuevers and regulations.  You'll learn how to be a more safe and precise pilot.  Enroll today at Alto Flight Academy and start your commercial pilot training in Oklahoma City.",
     title: "Commercial Pilot Training",
     hiddenField: "Commercial Pilot Flight Training",
     date: "Get paid to fly.  Complete your commercial pilot certificate!",
     price: "$1350",
     intro: [
-      "If you're looking to pursue a career in aviation and have a few hundred hours of flight experience, the next step after your instrument rating is obtaining a commercial pilot certificate. This allows you to fly for compensation or hire. Many pilots begin their careers with a commercial certificate, taking on roles such as flight instructing, surveying, and skydiving operations. For those aiming for an ATP certificate and an airline career, this is an ideal time to gain compensated flight experience.", 
+      "If you're looking to pursue a career in aviation and have a few hundred hours of flight experience, the next step after your instrument rating is obtaining a commercial pilot certificate. This allows you to fly for compensation or hire. Many pilots begin their careers with a commercial certificate, taking on roles such as flight instructing, surveying, and skydiving operations. For those aiming for an ATP certificate and an airline career, this is an ideal time to gain compensated flight experience.",
       "Commercial pilot training involves mastering regulations, understanding the FAR's limitations and requirements, and learning additional maneuvers with greater precision. You'll be expected to fly with higher accuracy, adhering to stricter standards than those for private pilots, as outlined in the ACS. To start your commercial pilot training at Alto Flight Academy, contact our team today. Explore our facilities, review the comprehensive syllabus, and take the first step towards becoming a more skilled and confident pilot.",
-      ],
+    ],
     content: [
       "Students are encouraged to complete their written exam before beginning commercial flight training or to be enrolled in a commercial ground school. During commercial flight training, you'll apply the advanced navigation and flight techniques covered in ground school. You'll learn about commercial regulations, flight maneuvers, and the precise flying required for commercial operations. You'll be paired with a certified flight instructor (CFI) to fine-tune your skills and help you become more proficient and accurate. Together, you and your CFI will develop a personalized plan that fits your learning style and schedule, ensuring you meet all the necessary requirements to obtain your commercial pilot certificate. The first step is reaching out to us by phone or filling out one of our contact forms to connect with an Alto Flight Academy team member. Give us a call today and take the next step towards mastering commercial flight!",
     ],
-    headerImage: "/src/assets/commercial-pilot-landing-grass-runway-in-cedar-mills.jpg",
+    headerImage:
+      "/src/assets/commercial-pilot-landing-grass-runway-in-cedar-mills.jpg",
     headerAlt: "Kayden Russell passed his commercial pilot exam",
     primaryImage:
       "/src/assets/commercial-pilot-passing-exam-at-alto-flight-oklahoma-city.jpg",
     primaryAlt: "Jordan Saxton passed his commercial pilot exam",
     secondaryImage: "/src/assets/student-passed-his-commercial-pilot-exam.jpg",
-    secondaryAlt: "Kayden Russell passed his commercial pilot exam",
+    secondaryAlt: "Alto Flight Academy student passing commercial checkride",
     prerequisites: [
-      "Complete necessary cross-country flight time (50 hours as Pilot in Command)", 
-      "Accumulate 250 total flight hours", 
-      "Master advanced flight maneuvers (steep turns, chandelles, lazy eights, etc.)", 
-      "Learn commercial regulations and requirements", 
-      "Gain proficiency in night flying and complex aircraft operations", 
-      "Conduct a long solo cross-country flight", 
-      "Receive endorsements from your CFI", 
-      "Complete the FAA commercial pilot practical exam"
-   ],
+      "Complete necessary cross-country flight time (50 hours as Pilot in Command)",
+      "Accumulate 250 total flight hours",
+      "Master advanced flight maneuvers (steep turns, chandelles, lazy eights, etc.)",
+      "Learn commercial regulations and requirements",
+      "Gain proficiency in night flying and complex aircraft operations",
+      "Conduct a long solo cross-country flight",
+      "Receive endorsements from your CFI",
+      "Complete the FAA commercial pilot practical exam",
+    ],
     process: [
       "Give us a call, fill out a form, or stop by",
       "Meet with an Alto Flight Academy team member",
