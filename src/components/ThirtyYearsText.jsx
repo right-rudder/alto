9330ab32720d1f49ec624fa0f8339e8f0d2098cc
Diego <diegoyon@outlook.com>
2024-10-26 17:43:36 -0600

    add eager loading to homepage header image

--- src/components/ThirtyYearsText.jsx
+++ src/components/ThirtyYearsText.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import CountUp from "react-countup";
 
 const ThirtyYearsText = () => {
   return (
-    <h2 class="text-center text-3xl lg:text-5xl pb-10">
+    <h2 className="text-center text-3xl lg:text-5xl pb-10">
       <CountUp end={30} duration={4} enableScrollSpy scrollSpyDelay={200} />
       <span>
-        &nbsp;years of <br class="lg:hidden" />
+        &nbsp;years of <br className="lg:hidden" />
         professional pilot training
       </span>
     </h2>
